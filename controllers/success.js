78566cf6cc717621e7e349ac4f2c44d50f230de9
Bichchuran10 <bichchuran10@gmail.com>
2023-02-14 00:57:40 +0530

    dynamic routes added

--- controllers/success.js
+++
@@ -1,7 +0,0 @@
-
-const path = require('path');
-
-const rootDir = require('../util/path');
-exports.getSuccessPage=(req,res,next)=>{
-    res.sendFile(path.join(rootDir,'views','success.html'))
-   }
--- routes/admin.js
+++ routes/admin.js
@@ -1,23 +1,18 @@
+const path = require('path');
 
 const express = require('express');
 
+const adminController = require('../controllers/admin');
+
 const router = express.Router();
 
-const productController=require('../controllers/product')
-
-
 // /admin/add-product => GET
+router.get('/add-product', adminController.getAddProduct);
 
-router.get('/add-product',productController.getAddProduct)
-router.post('/add-product',productController.postAddProduct)
-// router.get('/add-product', (req, res, next) => {
-//   res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
-// });
+// /admin/products => GET
+router.get('/products', adminController.getProducts);
 
-// // /admin/add-product => POST
-// router.post('/add-product', (req, res, next) => {
-//   console.log(req.body);
-//   res.redirect('/');
-// });
+// /admin/add-product => POST
+router.post('/add-product', adminController.postAddProduct);
 
 module.exports = router;
