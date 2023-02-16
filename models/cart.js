404b3a6f7f38d6a3f82054dc4eaaf12fbae2b4f1
Bichchuran10 <bichchuran10@gmail.com>
2023-02-16 18:24:12 +0530

    dynamic routes 3-edit,delete added

--- models/cart.js
+++ models/cart.js
@@ -15,7 +15,7 @@ module.exports=class Cart{
             if(!err){
                 cart=JSON.parse(fileContent)
             }
-        const existingProductIndex=cart.products.findIndex(prod=>id===id)
+        const existingProductIndex=cart.products.findIndex(prod=>prod.id===id)
         const existingProduct=cart.products[existingProductIndex]
         let updatedProduct;
         if(existingProduct)
@@ -28,11 +28,45 @@ module.exports=class Cart{
             updatedProduct={id:id, qty:1}
             cart.products=[...cart.products,updatedProduct]
         }
-        cart.totalPrice=cart.totalPrice+ +productPrice //+to add productPrice to number 
+        cart.totalPrice=cart.totalPrice+ +productPrice //+to add productPrice to number  
         fs.writeFile(p,JSON.stringify(cart),err=>{
             console.log(err)
         });
 
     }) 
+    }
+
+    static deleteProduct(id,productPrice){
+        fs.readFile(p,(err,fileContent)=>{ 
+
+            if(err){
+                return;
+            }
+            const updatedCart={...JSON.parse(fileContent)} 
+            const product=updatedCart.products.find(prod=>prod.id===id)
+            if(!product){
+                return;
+            }
+            const productQty=product.qty
+            updatedCart.products=updatedCart.products.filter(
+                prod=>prod.id!==id
+            )
+            updatedCart.totalPrice=updatedCart.productPrice-productPrice*productQty
+            fs.writeFile(p,JSON.stringify(updatedCart),err=>{
+                console.log(err)
+            });
+        })
+
+    }
+
+    static getCart(cb){
+        fs.readFile(p,(err,fileContent)=>{
+            const cart=JSON.parse(fileContent)
+            if(err){
+                cb(null)
+            } else{
+                cb(cart)
+            }
+        })
     }
 }
